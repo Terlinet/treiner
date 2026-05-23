6b55714f008814896a6bfbff0acd3bed77562f91
Terlinet <Terlinet@users.noreply.github.com>
2026-05-23 20:01:53 +0000

    deploy: 37c3f8611ad5fd97639b26a852ec27c96f31a076

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2420091794" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1979841248" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
